3a39a901ff8c1147162645a797981d110496e350
Subhajit Adhikari <subhajitadhikari90@gmail.com>
2025-11-02 10:02:05 +0530

    Speed Boost Ability Implemented

--- Assets/Scripts/Abilities/AbilitySpeedBoost.cs
+++ Assets/Scripts/Abilities/AbilitySpeedBoost.cs
@@ -1,9 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AbilitySpeedBoost : AbilityBase
 {
+    [SerializeField] private float boostedSpeed = 25f;
+    [SerializeField] private float duration = 5f;
+    private float defaulSpeed;
+
     internal override void OnAbilityUse(Ray ray, GameManager.Team team)
     {
+        BoostSpeed();
+    }
 
+    private void BoostSpeed()
+    {
+        NavMeshAgent agent = casterObject.GetComponent<NavMeshAgent>();
+        defaulSpeed = agent.speed;
+        agent.speed = boostedSpeed;
+        Invoke(nameof(ResetSpeed), duration);
+    }
+
+    private void ResetSpeed()
+    {
+        casterObject.GetComponent<NavMeshAgent>().speed = defaulSpeed;
     }
 }
